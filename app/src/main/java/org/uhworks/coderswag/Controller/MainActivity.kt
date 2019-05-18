23191c7c6d064b7c11d484080f69ae5582ec6feb
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-18 03:55:23 +0200

    Added ListView adapter and custom listview layout

--- app/src/main/java/org/uhworks/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/org/uhworks/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package org.uhworks.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import org.uhworks.coderswag.Model.Category
 import org.uhworks.coderswag.R
+import org.uhworks.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    // Create an adapter to serve the data to the listview
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,        // Context
+            android.R.layout.simple_list_item_1,    // Default view
+            DataService.categories)                 // Source of data
+
+        categoryListView.adapter = adapter
     }
 }
