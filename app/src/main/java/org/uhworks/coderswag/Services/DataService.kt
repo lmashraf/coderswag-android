070a6b776f841155922de88b17f920095d407e2f
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-19 14:55:32 +0200

    Add product list view for recycler view template

--- app/src/main/java/org/uhworks/coderswag/Services/DataService.kt
+++ app/src/main/java/org/uhworks/coderswag/Services/DataService.kt
@@ -6,31 +6,44 @@ import org.uhworks.coderswag.Model.Product
 object DataService {
 
     val categories = listOf(
-        Category("DIGITAL","digitalgoodsimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
         Category("HATS", "hatimage"),
-        Category("HOODIES","hoodieimage"),
-        Category("SHIRTS","shirtimage")
+        Category("HOODIES", "hoodieimage"),
+        Category("SHIRTS", "shirtimage")
     )
 
     val hats = listOf(
-        Product("Devslopes Graphic Beanie","$18", "hat01"),
-        Product("Devslopes Hat Black","$20", "hat02"),
-        Product("Devslopes Hat White","$18", "hat03"),
-        Product("Devslopes Hat Snapback","$22", "hat04")
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Devslopes Hoodie Grey","$28", "hoodie01"),
-        Product("Devslopes Hoodie Red","$32", "hoodie02"),
-        Product("Devslopes Grey Hoodie","$28", "hoodie03"),
-        Product("Devslopes Hoodie Black","$32", "hoodie04")
+        Product("Devslopes Hoodie Grey", "$28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Grey Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Hoodie Black", "$32", "hoodie4")
     )
 
-    val shirts= listOf(
-        Product("Devslopes Shirt Black","$18", "shirt01"),
-        Product("Devslopes Badge Light Grey","$20", "shirt02"),
-        Product("Devslopes Logo Shirt Red","$22", "shirt03"),
-        Product("Devslopes Hustle","$22", "shirt04"),
-        Product("Kickflip Studios","$18", "shirt05")
+    val shirts = listOf(
+        Product("Devslopes Shirt Black", "$18", "shirt1"),
+        Product("Devslopes Badge Light Grey", "$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devslopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "$18", "shirt5")
     )
+
+    val digitalgoods = listOf<Product>()
+
+    fun getProductList(category: String): List<Product> {
+
+        return when (category) {
+
+            "HATS" -> hats
+            "SHIRTS" -> shirts
+            "HOODIES" -> hoodies
+            else -> digitalgoods
+        }
+    }
 }
